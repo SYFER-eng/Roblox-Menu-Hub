8fc7a602817e8b31284feed221b3aef5ddcc1f5a
Syfer-eng <reviewedpuppy93@gmail.com>
2025-02-23 08:28:01 -0600

    Update Fly-Hack.lua

--- Scripts/Fly-Hack.lua
+++ Scripts/Fly-Hack.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SYFER-eng/Roblox-Menu-Hub/refs/heads/main/Loading/Fly.lua",true))()
 local player = game.Players.LocalPlayer
 local mouse = player:GetMouse()
 local tweenService = game:GetService("TweenService")
